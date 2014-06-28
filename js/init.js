3ae554a049d54d0dca91a7f6df32aab0b867ae88
anton <AntonAbramov1990@gmail.com>
2014-06-28 16:17:55 +0300

    fix table audio

--- js/init.js
+++ js/init.js
@@ -23,7 +23,6 @@ $(document).ready(function () {
 		$('.player .title').text(title);
 		$('.player .artist').text(artist);
 		$('.player .cover').css('background-image', 'url(data/' + cover + ')');
-		;
 
 		song = new Audio(url);
 
@@ -151,8 +150,6 @@ $(document).ready(function () {
 	})
 
 
-
-
 });
 
 $(window).resize(function () {
@@ -217,7 +214,6 @@ var galery = function () {
 	}
 
 
-
 }
 
 faqList = function () {
@@ -245,45 +241,42 @@ var modal = function () {
 }
 
 
-
 // Fires whenever a player has finished loading
 function onPlayerReady(event) {
-    event.target.playVideo();
+	event.target.playVideo();
 }
 
 // Fires when the player's state changes.
 function onPlayerStateChange(event) {
-    // Go to the next video after the current one is finished playing
-    if (event.data === 0) {
-        $.fancybox.next();
-    }
+	// Go to the next video after the current one is finished playing
+	if (event.data === 0) {
+		$.fancybox.next();
+	}
 }
 
 // The API will call this function when the page has finished downloading the JavaScript for the player API
 
-    // Initialise the fancyBox after the DOM is loaded
-$(document).ready(function() {
-    $(".fancybox")
-        .attr('rel', 'gallery')
-        .fancybox({
-            openEffect  : 'none',
-            closeEffect : 'none',
-            nextEffect  : 'none',
-            prevEffect  : 'none',
-            padding     : 0,
-            margin      : 50,
-            beforeShow  : function() {
-                // Find the iframe ID
-                var id = $.fancybox.inner.find('iframe').attr('id');
+// Initialise the fancyBox after the DOM is loaded
+$(document).ready(function () {
+	if ($('.video-item').length) {
+		$(".fancybox")
+			.attr('rel', 'gallery')
+			.fancybox({
+				beforeShow: function () {
+					// Find the iframe ID
+					var id = $.fancybox.inner.find('iframe').attr('id');
 
-                // Create video player object and add event listeners
-                var player = new YT.Player(id, {
-                    events: {
-                        'onReady': onPlayerReady,
-                        'onStateChange': onPlayerStateChange
-                    }
-                });
-            }
-        });
+					// Create video player object and add event listeners
+					var player = new YT.Player(id, {
+
+						events: {
+							'onReady': onPlayerReady,
+							'onStateChange': onPlayerStateChange
+						}
+					});
+				}
+			});
+	}
+
 });
 
